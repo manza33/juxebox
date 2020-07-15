b1c79f6f644c6798cc67cba6ba38f52c9e45fc33
Laetitia B <bourgois.laetitia@gmail.com>
2020-07-15 23:18:21 +0200

    Json arduino - php ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
 #include "led.h"
 #include "music.h"
 using namespace std;
@@ -14,17 +16,45 @@ using namespace std;
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
-String music;
-int num_music = 0;
-
+//Const connexion a la page
 const char* host = "www.manza3d.com";
 const uint16_t port = 80;
 WiFiClient client;
+DynamicJsonDocument doc(1024);
+
 
 const int buttonInput = 5;  // D1
 bool bButtonPressedEvent = false;
 bool bButtonPressedDown = false;
 bool bButtonPressedUp = false;
+bool countButtonPressed = false;
+//String music_json;
+//int num_music = 0;
+int i = 0;
+//String name = "";
+
+// ################## Musique reine des neiges  ################## 
+
+//durée en milliseconde pour chaque note (dans l'ordre)
+int PrincesseDesNeigesRytm[] = {
+    250, 250, 1500, 250, 250, 750,
+    250, 250, 750, 250, 750,
+    250, 750,
+    250, 250, 1500, 250, 250, 250, 250, 750,
+    250, 250, 750, 750, 500, 500, 750, 0};
+
+//Note à jouer (dans l'ordre)
+int PrincesseDesNeigesNote[] = {
+    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_G4, NOTE_G4, NOTE_D5,
+    NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4,
+    NOTE_B4, NOTE_C5,
+    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_G4, NOTE_G4, NOTE_E5, NOTE_E5, NOTE_D5,
+    NOTE_C5, NOTE_D5, NOTE_E5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_C5, 0};
+
+int MusiquePin = 14;
+
+// ################################################################# 
+
 
 // Checks if motion was detected, sets LED HIGH and starts a timer
 ICACHE_RAM_ATTR void buttonPressed() {
@@ -39,7 +69,7 @@ ICACHE_RAM_ATTR void buttonPressed() {
 void setup() {
 
 
-// We start by connecting to a WiFi network
+  // We start by connecting to a WiFi network
 
     Serial.println();
     Serial.print("Connecting to ");
@@ -76,8 +106,7 @@ unsigned long startTime = 0;
 uint8_t appuiSuccessif = 0;
 unsigned long elapseTimeAfterManyClick = 0;
 
-void loop() {
-  
+void loop() {  
     
     unsigned long now = millis();
     if (now - lastMsg > 1000) {
@@ -91,6 +120,9 @@ void loop() {
             
             Serial.println("APPUI COURT!!!");
             bButtonPressedDown = false;
+            countButtonPressed = true; 
+
+            // ################## Connexion a la page  ################## 
 
             if (client.connect(host, 80)) {  //starts client connection, checks for connection
                 Serial.println("connected");
@@ -98,75 +130,147 @@ void loop() {
                 client.println("Host: www.manza3d.com");
                 client.println("Connection: close");  //close 1.1 persistent connection 
                 client.println(); //end of get request
-            }else{
+            }
+            else{
                 Serial.println("connection failed");
             }
-
-            //GET /juxebox_server/ HTTP/1.1
-            //Host: www.manza3d.com
-
-            // This will send a string to the server
-            // Serial.println("sending data to server");
-            // if (client.connected()) {
-            //     client.println("hello from ESP8266");
-            // 
-
-              // wait for data to be available
-            // unsigned long timeout = millis();
-            // while (client.available() == 0) {
-            //     //Serial.println("Timeout loop");
-
-            //     if (millis() - timeout > 5000) {
-            //     Serial.println(">>> Client Timeout !");
-            //     client.stop();
-            //     delay(60000);
-            //     return;
-            //     }
-            // }
 
             // Read all the lines of the reply from server and print them to Serial
             Serial.println("receiving from remote server");
             // not testing 'client.connected()' since we do not need to send data here
+
+            String json = "";
+            boolean httpBody = false;
             while (client.connected()) {
-                
-
-                music = client.readStringUntil('\r');
-                //Serial.println("Client reader");
-                //music = client.read();
-                Serial.print(music);
-
+                String line = client.readStringUntil('\r');
+                if (!httpBody && line.charAt(1) == '{') {
+                    httpBody = true;
+                }
+                if (httpBody) {
+                    json += line;
+                }
             }
+            //StaticJsonBuffer<400> jsonBuffer;
+            Serial.println("Got data:");
+            Serial.println(json);
 
-            //Close the connection
+            
+            // Close the connexion
             Serial.println();
             Serial.println("closing connection");
             client.stop();  
 
-            //int music = rand() % 3 + 1;
+            //  ################## Fin Connexion  ################## 
 
-            Serial.print("Random music : ");
-            Serial.println(music);
-            num_music = atoi(music.c_str()); 
+            // ### parse JSON ###
 
-            switch (num_music) {
-                case 1:
-                    Serial.println("Fonction 1");
-                    PlayMario();
-                    break;
-                case 2:
-                    Serial.println("Fonction 2");
-                    PlayStarwars();
-                    break;
-                case 3:
-                    Serial.println("Fonction 3");
-                    break;
-                default:
-                    break;
-                
+            DeserializationError error = deserializeJson(doc, json);        
+
+            // Test if parsing succeeds.
+            if (error) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.c_str());
+                return;
             }
+
         }
-     
+
+        bButtonPressedEvent = false;     
     }
+
+    // switch (num_music)
+    // {
+    //     case 1:
+    //         Serial.println("Mario");
+    //         name = "Mario";
+    //         //PlayMario();
+    //         num_music = 0;
+
+    //         break;
+    //     case 2:
+    //         Serial.println("Star Wars");
+    //         name = "Star Wars";
+    //         //PlayStarwars();
+    //         num_music = 0;
+
+
+    //         break;
+    //     case 3:
+    //         Serial.println("Reine des neiges");
+    //         name = "Reine des neiges";
+
+    //     // Lecture du tableau de note ... lorsque nous lisons un 0  c'est que c'est la fin
+    //         if (PrincesseDesNeigesNote[i] != 0)
+    //         {
+    //             //On joue la note
+    //             tone(MusiquePin, PrincesseDesNeigesNote[i], PrincesseDesNeigesRytm[i]);
+    //             //On attend X millisecondes (durée de la note) avant de passer à la suivante
+    //             delay(PrincesseDesNeigesRytm[i]);
+    //             //On arrête la lecture de la note
+    //             noTone(MusiquePin);
+    //             //On marque une courte pose (entre chaque note, pour les différencer)
+    //             delay(50);
+    //             //On passe à la note suivante
+    //             ++i;
+    //         }
+    //         else
+    //         {
+    //             i = 0;
+    //         }
+
+    //         break;
+    //     default:
+    //         break;
+    // }
+
+    if (countButtonPressed == true)
+    {        
+
+
+        const char* name = doc["name"]; 
+        int note = doc["notes"][i]["note"];  
+        int tempo = doc["notes"][i]["tempo"];      
+        Serial.print("name : ");
+        Serial.println(name);
+        Serial.print("note : ");
+        Serial.println(note);
+        Serial.print("tempo : ");
+        Serial.println(tempo);
+        
+
+        // ##################  Music String to int #####################
+        // int note_int = atoi(note.c_str()); 
+        // Serial.print("note_int : ");
+        // Serial.println(note_int);
+        // Serial.print("Random music : ");
+        // Serial.println(num_music);
+
+        //music = rand() % 3 + 1;
+
+        if (tempo != -1)
+        {
+
+            //On joue la note
+            tone(MusiquePin, note, tempo);
+            //On attend X millisecondes (durée de la note) avant de passer à la suivante
+            delay(tempo);
+            //On arrête la lecture de la note
+            noTone(MusiquePin);
+            //On marque une courte pose (entre chaque note, pour les différencer)
+            delay(50);
+            //On passe à la note suivante
+            ++i;
+        }
+        else
+        {
+            i = 0;
+        }
+
+        //countButtonPressed = false; 
+
+
+    }
+
 }
 
 
